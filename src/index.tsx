d4073a17119ccc78a7ea658952bf2e9276a104a9
GaryGolf <garygolf@mail.ru>
2018-07-01 20:47:59 +0300

    create test component

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+interface Props {}
+
+export default class ReactStickyList extends React.PureComponent<Props, null> {
+  render() {
+    return (
+      <h1>React Sticky List Component</h1>
+    )
+  }
+}
