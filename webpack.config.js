3d39bf5c43fda87e036f103d4da97ec29fe49155
GaryGolf <garygolf@mail.ru>
2018-07-01 20:43:14 +0300

    add dependencies

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const mode = process.env.NODE_ENV;
+const PRODUCTION = mode == 'production';
+
+const prodCilentConfig = {
+  mode: 'production',
+  entry: './src/client.tsx',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2'
+  },
+  
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+
+  devtool: false,
+
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+      { test: /\.css$/, use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: {
+          loader: 'css-loader',
+          query: {
+            modules: true,
+            importLoaders: 1,
+            localIdentName: '[local]__[hash:base64:5]'
+          }
+        }
+      })}
+    ]
+  },
+
+  plugins: [
+    new ExtractTextPlugin("styles.css"),
+    new CleanWebpackPlugin(['dist']),
+    new UglifyJsPlugin(),
+    new webpack.DefinePlugin({
+      PRODUCTION: JSON.stringify(true)
+    })
+  ]
+};
+
+module.exports = prodCilentConfig;
