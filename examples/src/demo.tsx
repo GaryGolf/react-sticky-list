2569d0fc87826c8c4a28e0d4d54ea91f1a28305e
GaryGolf <garygolf@mail.ru>
2018-07-13 21:56:05 +0300

    update demo

---
+++ examples/src/demo.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import StickyList from '../../src/sticky-list';
+
+interface Props {}
+
+export default class Demo extends React.PureComponent<{}, {}> {
+  render() {
+    return (
+      <StickyList>
+        <h1>React Sticky List</h1>
+        <div>Hello 1</div>
+      </StickyList>
+    )
+  }
+}
