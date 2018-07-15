03a29bc726df094e2c74e063e09a2c288726f5e8
GaryGolf <garygolf@mail.ru>
2018-07-15 17:25:58 +0300

    update demo component

--- examples/src/demo.tsx
+++ examples/src/demo.tsx
@@ -1,70 +1,49 @@
 import * as React from 'react';
 import StickyList from '../../src/sticky-list';
 
+import * as json from './pizza.json'
+import { access } from 'fs';
+
 interface Props {}
 
+interface MenuItem {
+  title: string;
+  description: string;
+  price: number;
+  novelty?: boolean;
+}
+interface Menu {
+  header: string;
+  items: MenuItem[];
+}
+
 export default class Demo extends React.PureComponent<{}, {}> {
+
+  private renderMenu = () => {
+    const menu:JSX.Element[] = [];
+    if(!json || !json.length) return menu;
+    return json.reduce((acc:JSX.Element[],menu:Menu) => {
+      acc.push(<header key={menu.header}>{menu.header}</header>)
+      const a = menu.items.map(item => (
+        <div key={item.title}>{item.title}....{item.price}</div>
+      ))
+      return [...acc, ...a];
+    },[])
+  }
+
   render() {
+    const menu = this.renderMenu();
+    // console.log(menu)
+
     return (
       <StickyList>
-        <h1>React Sticky List</h1>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <h1>React Sticky List</h1>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <h1>React Sticky List</h1>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <h1>React Sticky List</h1>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-        <div>Hello 1</div>
-
+       {menu}
       </StickyList>
     )
+    // return (
+    //   <div>
+    //     {menu}
+    //   </div>
+    // )
   }
 }
