971284525d3fd5ef35dc53b7efc8bb5ca971a96c
GaryGolf <garygolf@mail.ru>
2018-07-16 13:55:57 +0300

    minor changes

--- examples/src/demo.tsx
+++ examples/src/demo.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import StickyList from '../../src/sticky-list';
 
 import * as json from './pizza.json'
-import { access } from 'fs';
+
 
 interface Props {}
 
@@ -23,11 +23,11 @@ export default class Demo extends React.PureComponent<{}, {}> {
     const menu:JSX.Element[] = [];
     if(!json || !json.length) return menu;
     return json.reduce((acc:JSX.Element[],menu:Menu) => {
-      acc.push(<header key={menu.header}>{menu.header}</header>)
-      const a = menu.items.map(item => (
+      const header = <header key={menu.header}>{menu.header}</header>;
+      const items = menu.items.map(item => (
         <div key={item.title}>{item.title}....{item.price}</div>
       ))
-      return [...acc, ...a];
+      return [...acc, header, ...items];
     },[])
   }
 
